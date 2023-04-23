1d8c99af0a4e3916fd7ec22bbb0287965106c38c
Bartosz-Zarski <117305195+Bartosz-Zarski@users.noreply.github.com>
2023-04-23 19:34:28 +0200

    Test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,9 @@
 const buttonStart = document.querySelector('button[data-start]');
 const buttonEnd = document.querySelector('button[data-stop]');
 
+let timerId = null
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
